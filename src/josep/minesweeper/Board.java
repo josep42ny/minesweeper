dc7b9d6e1ac0fdf5f94b72c3904282c065726277
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-02-26 13:30:24 +0100

    add tiles and its getter to board

--- src/josep/minesweeper/Board.java
+++ src/josep/minesweeper/Board.java
@@ -4,10 +4,15 @@ public class Board {
 
     private int sizeX;
     private int sizeY;
+    private Tile[][] tiles;
 
     public Board(int sizeX, int sizeY) {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
+    }
+
+    public Tile[][] getTiles() {
+        return tiles;
     }
 
 }
