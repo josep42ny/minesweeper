0694b558204ba03c73bf8bf3095463ae9c30ed7f
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-02-26 13:27:14 +0100

    add rough Tile methods and fields

--- src/josep/minesweeper/Tile.java
+++ src/josep/minesweeper/Tile.java
@@ -2,4 +2,41 @@ package josep.minesweeper;
 
 public abstract class Tile {
 
+    private boolean mine;
+    private boolean flagged;
+    private boolean revealed;
+    private int value;
+
+    public Tile(boolean isMine) {
+        this.mine = true;
+    }
+
+    public Tile(int value) {
+        this.value = value;
+    }
+
+    public boolean isMine() {
+        return mine;
+    }
+
+    public boolean isFlagged() {
+        return flagged;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setRevealed(boolean revealed) {
+        this.revealed = revealed;
+    }
+
+    public void setFlagged(boolean flagged) {
+        this.flagged = flagged;
+    }
+
 }
