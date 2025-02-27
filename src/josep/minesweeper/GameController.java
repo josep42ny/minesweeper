442aa16f24f62b8a2d659b0cab4d7e7dac202c7c
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-02-27 12:52:02 +0100

    add gameController working outline

--- src/josep/minesweeper/GameController.java
+++ src/josep/minesweeper/GameController.java
@@ -4,22 +4,52 @@ public class GameController {
 
     private View view;
     private Board board;
-
-    GameController(){
-
-        this.view = new View();
-        this.board = new Board(8,8);
-
-    }
+    private InputHandler inputHandler;
 
     public void awake() {
 
-        view.drawBoard(board);
+        this.view = new View();
+        this.board = new Board(8,8);
+        this.inputHandler = new InputHandler();
+        update();
 
     }
 
     private void update() {
 
+        while (true) {
+
+            view.drawBoard(board);
+            PlayerAction action = inputHandler.askForAction();
+            int[] coords;
+            switch (action) {
+                case REVEAL:
+                    do {
+                        coords = inputHandler.askForCoordinates();
+                    } while (board.isTileRevealed(coords));
+                    boolean mine = board.reveal(coords);
+                    if (mine) {
+                        gameover();
+                    }
+                    break;
+                case FLAG:
+                    do {
+                        coords = inputHandler.askForCoordinates();
+                    } while (board.isTileRevealed(coords));
+                    board.flag(coords);
+                    break;
+                case QUIT:
+                    System.exit(0);
+                default:
+            }
+
+        }
+
+    }
+
+    private void gameover() {
+        board.revealAll();
+        System.exit(0);
     }
 
 }
